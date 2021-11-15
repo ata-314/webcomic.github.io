3dcc7fbf2f34f4f7c7ef185f06fe8054e97a8fd7
ata-314 <83098505+ata-314@users.noreply.github.com>
2021-11-15 11:39:50 +0300

    web comig

---
+++ 1.bölüm.js
@@ -0,0 +1,9 @@
+const link = document.querySelector('.box')
+link.style.display="none"
+
+function bigImg() {
+    link.style.display=""
+  }
+  function bigImg2() {
+    link.style.display="none"
+  }
